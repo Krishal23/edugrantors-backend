61629fb76a90c4089d042ebe3473c83e68a0888f
Kushal Kesharwani <kanhakesharwani23@gmail.com>
2026-06-25 02:26:54 +0530

    updated origin

--- app.ts
+++ app.ts
@@ -49,6 +49,7 @@ app.use(
       "https://edugrantors.vercel.app",
       "https://edugrantors-frontend.vercel.app",
       "https://www.edugrantors.in",
+      "https://edugrantors-frontend-nine.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
